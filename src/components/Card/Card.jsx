238466c99486d958851ba49f2e1281a7f743c011
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-03 13:05:38 +0200

    Lesson_3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,22 +9,23 @@
 //  Писать нужно на React, используя изученный на лекции синтаксис jsx.
 // Затем нужно импортировать компонет Card в компонент Homework02, где переиспользовать его 3 раза.
 // В App.jsx отобразить компонент Homework02.
-import { data } from "./data";
+// import { data } from "./data";
 import "./styles.css";
 
-function Card() {
+//props обєкт.с помощью которого передаем даніе из род. в дочерний
+function Card({avatarURL= "",firstName= "Name",lastName ="Last Name",occupation= "_",hobby="_",}) {
   return (
     <div>
       <div className="sympson_card">
         <img
           className="avatar_sympson"
-          src={data.avatarUrl}
+          src={avatarURL}
           alt="Foto Sypmson"
         />
-        <p className="first_name">FirstName: {data.firstName}</p>
-        <p className="last_name">LastName: {data.lastName} </p>
-        <p className="occupation">Occupation: {data.Occupation}</p>
-        <p className="hobby">Hobby: {data.Hobby} </p>
+        <p className="card_info">FirstName:{firstName} </p>
+        <p className="card_info">LastName:{lastName} </p>
+        <p className="card_info">Occupation:{occupation}</p>
+        <p className="card_info">Hobby:{hobby}</p>
       </div>
     </div>
   );

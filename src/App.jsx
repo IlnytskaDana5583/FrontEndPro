91b53e5807a437ca09879c0b83ae676eb74cdeb8
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-05 22:28:46 +0200

    Homework_04

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Homework_2 from "./homeworks/Homework_2/Homework_2";
 import Lesson_3 from "./lessons/Lesson_03/Lesson_03";
 import Homework_03 from "./homeworks/Homework_03/Homework_03";
 import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
+import Homework_04 from "./homeworks/Homework_04/Homework_04";
+
 
 
 
@@ -23,6 +25,7 @@ function App() {
       {/* {Homeworks} */}
       {/* <Homework_2/> */}
       {/* <Homework_03 /> */}
+      <Homework_04 />
       
       
       
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -21,7 +21,7 @@ import "./styles.css";
 function Button({ type = "button", name = "Send", onClick = () => {}, children }) {
   return (
     <button className="button_component" onClick={onClick} type={type}>
-      {/* {children ? "" : buttonName} */}
+      {/* children ? "" : buttonName */}
       {/* Улосвный рендеринг */}
       {!children && name}
       {children}

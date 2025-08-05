3c26f9eb7f24795ef5313db8746a493df152f7d8
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-05 09:22:00 +0200

    Lesson_04

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Lesson_02 from "./lessons/Lesson_2/Lesson_02"; //Lesson
 import Homework_2 from "./homeworks/Homework_2/Homework_2";
 import Lesson_3 from "./lessons/Lesson_03/Lesson_03";
 import Homework_03 from "./homeworks/Homework_03/Homework_03";
-import Lesson_04 from "./components/Lesson_04/Lesson_04";
+import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
+
+
+
 
 
 
@@ -17,11 +20,13 @@ function App() {
       {/* {Lesson_02} */}
       {/* <Lesson_02 /> */}
       {/* <Lesson_3/> */}
+      <Lesson_04 />
+
 
 
       {/* {Homeworks} */}
       {/* <Homework_2/> */}
-      <Homework_03 />
+      {/* <Homework_03 /> */}
       
       
       

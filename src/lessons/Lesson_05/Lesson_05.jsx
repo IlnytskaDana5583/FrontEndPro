e30f3ca0568bfc5a16df9973dbd9c719429d4e34
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-07 08:42:07 +0200

    Update Lesson_05

--- src/lessons/Lesson_05/Lesson_05.jsx
+++ src/lessons/Lesson_05/Lesson_05.jsx
@@ -1,42 +1,90 @@
-import Button from "../../components/Button/Button";
-import {useState} from "react";
+import { useState } from "react";
 import {v4} from "uuid";
+
+import Button from "../../components/Button/Button";
+
 import "./styles.css";
+
 function Lesson_05() {
-    let [orders,setOrders] =  useState([]);
+  let [orders, setOrders] = useState([]);
+
+  //   ["Burder", "Salad"]
+  const orderList = orders.map((orderItem) => {
+    return <li key={v4()} className="order_item">{orderItem}</li>;
+  });
+
+  console.log(orderList);
   return (
     <div className="lesson_05_page_wrapper">
       <div className="menu_wrapper">
         <h1 className="menu">Menu:</h1>
         <div className="button_wrapper">
           <div className="button_control">
-            <Button name="Burger" onClick={() => {
-                setOrders()
-            }} />
+            <Button
+              name="Burger"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Burger"];
+                });
+              }}
+            />
           </div>
           <div className="button_control">
-            <Button name="Fries" onClick={() => {}} />
+            <Button
+              name="Fries"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Fries"];
+                });
+              }}
+            />
           </div>
           <div className="button_control">
-            <Button name="Cola" onClick={() => {}} />
+            <Button
+              name="Cola"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Cola"];
+                });
+              }}
+            />
           </div>
           <div className="button_control">
-            <Button name="Salad" onClick={() => {}} />
+            <Button
+              name="Salad"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Salad"];
+                });
+              }}
+            />
           </div>
           <div className="button_control">
-            <Button name="Ketchup" onClick={() => {}} />
+            <Button
+              name="Ketchup"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Ketchup"];
+                });
+              }}
+            />
           </div>
           <div className="button_control">
-            <Button name="Ice-cream" onClick={() => {}} />
+            <Button
+              name="Ice-cream"
+              onClick={() => {
+                setOrders((prevValue) => {
+                  return [...prevValue, "Ice-Cream"];
+                });
+              }}
+            />
           </div>
         </div>
       </div>
+
       <div className="order_wrapper">
         <p className="order_title">Your Order:</p>
-        <ol className="order_list">
-          {/* <li className="order_item">Burger</li> */}
-          {/* <li className="order_item">Ice-creem</li> */}
-        </ol>
+        <ol className="order_list">{orderList}</ol>
       </div>
 
 

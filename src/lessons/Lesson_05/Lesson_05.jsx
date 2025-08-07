0e3bb83c93df9ed896db23bfc9baf2bdc29f5177
IlnytskaDana5583 <volodere3@gmail.com>
2025-08-07 07:47:00 +0200

    Lesson_5

---
+++ src/lessons/Lesson_05/Lesson_05.jsx
@@ -0,0 +1,48 @@
+import Button from "../../components/Button/Button";
+import {useState} from "react";
+import {v4} from "uuid";
+import "./styles.css";
+function Lesson_05() {
+    let [orders,setOrders] =  useState([]);
+  return (
+    <div className="lesson_05_page_wrapper">
+      <div className="menu_wrapper">
+        <h1 className="menu">Menu:</h1>
+        <div className="button_wrapper">
+          <div className="button_control">
+            <Button name="Burger" onClick={() => {
+                setOrders()
+            }} />
+          </div>
+          <div className="button_control">
+            <Button name="Fries" onClick={() => {}} />
+          </div>
+          <div className="button_control">
+            <Button name="Cola" onClick={() => {}} />
+          </div>
+          <div className="button_control">
+            <Button name="Salad" onClick={() => {}} />
+          </div>
+          <div className="button_control">
+            <Button name="Ketchup" onClick={() => {}} />
+          </div>
+          <div className="button_control">
+            <Button name="Ice-cream" onClick={() => {}} />
+          </div>
+        </div>
+      </div>
+      <div className="order_wrapper">
+        <p className="order_title">Your Order:</p>
+        <ol className="order_list">
+          {/* <li className="order_item">Burger</li> */}
+          {/* <li className="order_item">Ice-creem</li> */}
+        </ol>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default Lesson_05;
